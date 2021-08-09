b07f38f397f48e42fde7320b88213409d2452afa
GenykS <aquastep14@gmail.com>
2021-08-10 00:00:34 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,44 @@
+"use strict";
+
+// const obj = {
+//     firstName: 'John',
+//     age: 26,
+//     married: false
+
+// };
+
+// // console.log('My name ' + obj.firstName);
+// // console.log(obj);
+
+// const arr = ['plum', 'peach', 'apple'];
+
+// console.log(arr[1]);
+
+// alert ('Hello');
+
+// const result = confirm ('Are you here?');
+// console.log( result );
+
+// const answer = +prompt ('How old are you?', '18');
+// console.log (answer + 10);
+
+// lesson 5
+
+// const user = 'Ivan';
+
+// alert(`Hello ${user}`);
+
+
+// Объекты
+
+let incr = 10,
+    decr = 5;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+
+
